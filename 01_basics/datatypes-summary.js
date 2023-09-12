036432ce2e4638d5d5c5baab1d3e427e3b148298
Chaitanyakrishna Hanumantarao chundiwar <98137979+chaitanya-chundiwar@users.noreply.github.com>
2023-09-12 22:52:59 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3 
+
+const isLogggedIn = false
+const outsideTemp = null
+let userEmail;//undefined 
+
+const id = Symbol("123")
+const anotherid = Symbol("123")
+//both are not same
+console.log(id === anotherid)
+
+//const bigNumber = 1235656888373364363334n
+
+//Reference (Non primitives)
+
+// Array, Objects, Functions
+const heros = ["sakthiman", "krish" , "hanuman", "bhim"];
+
+// object is in {} 
+let myObject = {
+    name: "chaitanya",
+    age: 22,
+}
+
+//funtion can be declared in variable
+const myFuntion = function(){
+    console.log("Hello World!")
+}
+
+console.log(typeof outsideTemp)
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3

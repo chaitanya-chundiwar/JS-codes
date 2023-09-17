1d7292a2bf4291f33d74dcd9ffc153fd84138ec2
Chaitanyakrishna Hanumantarao chundiwar <98137979+chaitanya-chundiwar@users.noreply.github.com>
2023-09-17 22:08:45 +0000

    Maths & Num

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -45,5 +45,18 @@ console.log(typeof outsideTemp)
 let myYoutubename = "chaitanyadotnetcom"
 
 let anothername = myYoutubename
+anothername = "coffeeaurcode"
 
-console.log("anothername");
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "chaitanya@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);

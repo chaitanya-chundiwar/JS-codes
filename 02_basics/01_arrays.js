73334b0f015abafd68156d860ce382942837a16e
Chaitanyakrishna Hanumantarao chundiwar <98137979+chaitanya-chundiwar@users.noreply.github.com>
2023-10-22 14:35:23 +0000

    array

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// array
+
+const myArr = [0, 1, 2, 3 , 4, 5]
+const myHeros = ["krishna","ramarao","ram","arjuna","karna"]
+
+const myArr2 = new Array(1,2,3,4,6);
+// console.log(myArr2[2]);
+
+//Array methods
+// myArr.push(7);
+// myArr.push(11);
+//myArr.pop()
+//console.log(myArr);
+
+// myArr.unshift(12)
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(newArr)
+
+//slice , splice 
+
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+console.log("B", myArr);
+
+const myn2 = gmyArr.splice(1,3);
+console.log(myn2)
+
+console.log("C", myArr)

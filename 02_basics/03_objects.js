e2a2fce4404020bee2b795be5dd21769ff5cb1e3
Chaitanyakrishna Hanumantarao chundiwar <98137979+chaitanya-chundiwar@users.noreply.github.com>
2023-10-22 20:26:57 +0000

    object literals basics

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,45 @@
+//constructor as Singleton 
+//Object are declared as literals or constructor 
+
+//Object.create
+
+const mySym = Symbol("key1")
+
+//1 Object literal
+const JSUser = {
+Name : 'chaitanya',
+"full name": "chaitanya krishna",
+age : 26,
+[mySym] : "mykey1",
+location : "Dallas, Tx",
+email : "chaitanya@gmail.com",
+isLoggedIn: false,
+lastLoginDays: ["monday", "saturday"]
+}
+
+//2 ways
+// console.log(JSUser.email)
+// console.log(JSUser["email"])
+//console.log(JSUser["full name"])
+
+//console.log(JSUser[mySym])
+
+JSUser.email = "krishan@gmail.com"
+
+//console.log(JSUser.email) // changes are made
+
+ //Object.freeze(JSUser)
+ JSUser.email = "krishana@microsoft.com"
+// console.log(JSUser);
+
+// function can also be treated as variable
+JSUser.greeting = function(){
+console.log("Hello JS User")
+}
+JSUser.greetingTwo = function(){
+    console.log(`Hello JS User, ${this.Name}`)
+ }
+console.log(JSUser.greeting());
+console.log(JSUser.greetingTwo());
+
+
